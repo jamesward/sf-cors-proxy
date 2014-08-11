7935f0993e1c55e8aba2c9f0e68edaa45ae860ed
James Ward <james@jamesward.org>
2014-08-11 11:52:06 -0600

    more tests and / OPTIONS handler

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,11 +1,9 @@
-package controllers
-
+import org.scalatestplus.play._
 import play.api.http.HeaderNames
-import play.api.libs.json.{JsValue, Json, JsObject}
+import play.api.libs.json.{JsObject, JsValue, Json}
 import play.api.libs.ws.WS
 import play.api.mvc.Results
 import play.api.test.Helpers._
-import org.scalatestplus.play._
 
 class ApplicationSpec extends PlaySpec with Results with OneServerPerSuite {
 
@@ -21,6 +19,7 @@ class ApplicationSpec extends PlaySpec with Results with OneServerPerSuite {
       val response = await(ws("/services/data/v30.0/query/?q=SELECT+name+from+Contact").get())
       response.status mustEqual OK
       (response.json \ "records").as[Seq[JsObject]].length must be > 0
+      response.header(HeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN) mustBe 'defined
     }
   }
 
@@ -30,6 +29,7 @@ class ApplicationSpec extends PlaySpec with Results with OneServerPerSuite {
       val response = await(ws("/services/data/v30.0/sobjects/Contact/").post(json))
       response.status mustEqual CREATED
       (response.json \ "id").asOpt[String] mustBe 'defined
+      response.header(HeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN) mustBe 'defined
     }
   }
 
