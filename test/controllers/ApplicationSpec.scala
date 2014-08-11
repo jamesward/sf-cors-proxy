4efe6c1ed758397899a66e440846bb7e4f423838
James Ward <james@jamesward.org>
2014-08-11 10:35:28 -0600

    add tests

---
+++ test/controllers/ApplicationSpec.scala
@@ -0,0 +1,45 @@
+package controllers
+
+import play.api.http.HeaderNames
+import play.api.libs.json.{JsValue, Json, JsObject}
+import play.api.libs.ws.WS
+import play.api.mvc.Results
+import play.api.test.Helpers._
+import org.scalatestplus.play._
+
+class ApplicationSpec extends PlaySpec with Results with OneServerPerSuite {
+
+  def ws(path: String) = {
+    val maybeForceToken = sys.env.get("FORCE_TOKEN")
+    assume(maybeForceToken.isDefined)
+    val url = s"http://localhost:$port" + path
+    WS.url(url).withHeaders(HeaderNames.AUTHORIZATION -> s"Bearer ${maybeForceToken.get}")
+  }
+
+  "query contacts" must {
+    "return the contacts" in {
+      val response = await(ws("/services/data/v30.0/query/?q=SELECT+name+from+Contact").get())
+      response.status mustEqual OK
+      (response.json \ "records").as[Seq[JsObject]].length must be > 0
+    }
+  }
+
+  "create contact with required fields" must {
+    "create a contact" in {
+      val json = Json.obj("LastName" -> "Foo")
+      val response = await(ws("/services/data/v30.0/sobjects/Contact/").post(json))
+      response.status mustEqual CREATED
+      (response.json \ "id").asOpt[String] mustBe 'defined
+    }
+  }
+
+  "create contact without required fields" must {
+    "fail" in {
+      val json = Json.obj()
+      val response = await(ws("/services/data/v30.0/sobjects/Contact/").post(json))
+      response.status mustEqual BAD_REQUEST
+      response.json.as[Seq[JsValue]].headOption.flatMap(jv => (jv \ "errorCode").asOpt[String]) mustEqual Some("REQUIRED_FIELD_MISSING")
+    }
+  }
+
+}
