1e926080418b66e4c0134d8616376a96748ed223
James Ward <james@jamesward.org>
2014-08-12 14:18:36 -0600

    add Target-Endpoint to allowed headers

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -37,7 +37,7 @@ object Application extends Controller {
 
   def options(path: String) = CorsAction {
     Action { request =>
-      Ok.withHeaders(ACCESS_CONTROL_ALLOW_HEADERS -> Seq(AUTHORIZATION, CONTENT_TYPE).mkString(","))
+      Ok.withHeaders(ACCESS_CONTROL_ALLOW_HEADERS -> Seq(AUTHORIZATION, CONTENT_TYPE, "Target-Endpoint").mkString(","))
     }
   }
 
